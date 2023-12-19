1802ba7fc3b75de3ea005614a284cee22b39efe0
RubenD <rubendariopm14@gmail.com>
2023-12-19 00:04:52 -0500

    updating

---
+++ apps/chunk/comps/UserPhotos.svelte
@@ -0,0 +1,71 @@
+<script>
+    import { stringToColour } from "@utils/utils";
+    import { user_data } from "../store";
+	import { user_claims } from "@stores/user";
+
+    export let chunk;
+
+    let user_photos = [];
+    $: {
+        if (chunk?.props?.access?.length) {
+            const access_map = { Read: 1, Write: 2, Admin: 3 };
+            let access = Object.fromEntries(
+                chunk.props.access
+                    .map(({ user, access }) => [user, access])
+                    .sort((a, b) => access_map[b[1]] < access_map[a[1]]),
+            );
+            let users = new Set(chunk.props.access.map(({ user, access }) => user));
+            users.add(chunk.owner); // Add chunk owner
+            users.delete(user_claims.user); // Remove ourselves
+            users = user_data
+                .get_photos([...users].sort())
+                .sort((a, b) => !a.photo && !!b.photo);
+            // Rehydrate the users with their access
+            users = users.map((u) => ({
+                ...u,
+                access:
+                    u.user === chunk.owner ? "Admin" : access[u.user] ?? "Read",
+            }));
+            user_photos = users;
+        }
+    }
+    function user_decorator(access) {
+        return `outline: 1px ${
+            { Read: "none", Write: "dashed", Admin: "solid" }[access]
+        } currentColor;`;
+    }
+</script>
+
+{#each user_photos as { user, photo, access }, i}
+    {#if i <= 3}
+        {#if i === 3 && user_photos.length > 4}
+            <div
+                style="font-size:12px;width: 20px;height:20px;text-align:center"
+            >
+                +
+            </div>
+        {:else if photo}
+            <img
+                src={`/media/${photo}?max=21x21`}
+                alt={`${user}'s photo`}
+                style={`border-radius:99px;width: 21px;height:21px;${user_decorator(
+                    access,
+                )}`}
+                srcset={`/media/${photo}?max=21x21,
+                    /media/${photo}?max=42x42 2x,
+                    /media/${photo}?max=84x84 3x
+                    `}
+            />
+        {:else}
+            <div
+                style={`width: 21px;height:21px;border-radius:99px;background:${stringToColour(
+                    user,
+                )}66;font-size:14px;font-weight:bold;text-align:center;${user_decorator(
+                    access,
+                )}`}
+            >
+                {user.charAt(0).toUpperCase()}
+            </div>
+        {/if}
+    {/if}
+{/each}
