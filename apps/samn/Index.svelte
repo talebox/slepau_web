a90f508a7feeef15ad4c66b9e872dcae81039b1c
RubenD <rubendariopm14@gmail.com>
2024-03-25 23:59:24 -0400

    fixing month on vreji

--- apps/samn/Index.svelte
+++ apps/samn/Index.svelte
@@ -1,10 +1,32 @@
 <script>
 	import { Router, Route, Link, navigate } from "@deps/routing";
 
+	let last = false;
+	let res = Promise.resolve("nothing yet");
+	function toggle() {
+		last = !last;
+		res = fetch("/command/wait", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				for_id: 14,
+				command: { SetActuator: { id: 0, value: { Light: last } } },
+			}),
+		}).then(v => v.json());
+	}
 </script>
 
 
-<Router basepath="app">
+<button on:click={toggle}>Toggle</button>
 
-</Router>
+{#await res}
+	Waiting...
+{:then res}
+	Got: {res}
+{/await}
 
+<!-- <Router basepath="app">
+	<Route>
+		
+	</Route>
+</Router> -->
